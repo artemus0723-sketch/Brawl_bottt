c7f26b036c18026888ea2c853339587160a4e0aa
artemus0723-sketch <artemus07.23@gmail.com>
2026-09-24 09:49:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('8895895178:AAHRB6dt-LR_17MX9pMsNJZJDPqGruQHYzk')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Бот репозитория Brawl_bottt работает!")
+    bot.reply_to(message, "Привет,здесь ты можешь продать💸 свой акаунт в Бравл Старс быстро")
 
 if __name__ == '__main__':
     bot.infinity_polling()
